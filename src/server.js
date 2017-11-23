2455066ca92c007ad2f3300295c3ce10765e3c32
gnom <you@example.com>
2017-11-23 21:27:46 +0200

    server changes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const Koa = require('koa')
-const app = new Koa()
+const Router = require('koa-router')
+const BodyParser = require('koa-bodyparser')
 
-app.use(function(ctx) {
+const app = new Koa()
+const router = new Router()
+
+app.use(BodyParser())
+
+router.get('/', function(ctx) {
     ctx.body = 'hello, world!'
 })
+
+router.get('/items', function(ctx) {
+    ctx.body = 'items'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
 
